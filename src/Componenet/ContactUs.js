4527d01b8692bbabaea288821303bfc288ee520b
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2023-12-19 01:21:32 +0530

    contact validate

--- src/Componenet/ContactUs.js
+++ src/Componenet/ContactUs.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+// import { useLocation } from 'react-router-dom';
 
 export default function ContactUs() {
   const serverURL = 'http://localhost:5000/submit/post';
   const [user, setUser] = useState({ name: "", email: "", phone: "", description: "" });
+  // const location = useLocation();
 
   let value;
 
@@ -32,29 +34,29 @@ export default function ContactUs() {
 
   return (
     // <div className='contactus'>
-      <form >
-        <div className="contactbox">
-          <div className='my'>
-            <div><label htmlFor='name'>NAME</label></div>
-            <div><input type="text" name="name" id="name" onChange={handleInput} /></div>
-          </div>
-          <div className='my'>
-            <div><label htmlFor='email'>EMAIL</label></div>
-            <div><input type="email" name="email" id="email" onChange={handleInput} /></div>
-          </div>
-          <div className='my'>
-            <div><label htmlFor='phone'>PHONE NO</label></div>
-            <div><input type="number" name="phone" id="phone" onChange={handleInput} /></div>
-          </div>
-          <div className='my'>
-            <div><label htmlFor='description'>DESCRIPTION</label></div>
-            <div><textarea name="description" id="description" rows="3" onChange={handleInput}> </textarea></div>
-          </div>
-          <div className='my'>
-            <button type="submit" style={{ cursor: "pointer" }} onClick={postData}>SUBMIT</button>
-          </div>
+    <form >
+      <div className="contactbox">
+        <div className='my'>
+          <div><label htmlFor='name'>NAME</label></div>
+          <div><input type="text" name="name" id="name" onChange={handleInput} minLength={3} required /></div>
         </div>
-      </form>
+        <div className='my'>
+          <div><label htmlFor='email'>EMAIL</label></div>
+          <div><input type="email" name="email" id="email" onChange={handleInput} required /></div>
+        </div>
+        <div className='my'>
+          <div><label htmlFor='phone'>PHONE NO</label></div>
+          <div><input type="tel" name="phone" id="phone" onChange={handleInput} pattern="[0-9]{10}" required /></div>
+        </div>
+        <div className='my'>
+          <div><label htmlFor='description'>DESCRIPTION</label></div>
+          <div><textarea name="description" id="description" rows="3" onChange={handleInput} minLength={10} required > </textarea></div>
+        </div>
+        <div className='my'>
+          <button type="submit" onClick={postData}>SUBMIT</button>
+        </div>
+      </div>
+    </form>
     // </div>
   )
 }
